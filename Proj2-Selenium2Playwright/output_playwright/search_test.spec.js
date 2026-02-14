3bcb3c5097b81a2f1377ad8d15e3c6be1ea53c33
Sakharsukh <sakharsukh@gmail.com>
2026-02-14 11:12:29 +0530

    commit

--- Proj2-Selenium2Playwright/output_playwright/search_test.spec.js
+++ Proj2-Selenium2Playwright/output_playwright/search_test.spec.js
@@ -2,10 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('search_test.py conversion', async ({ page }) => {
     // Converted Code Start
-    page.locator('#login').click();
-    page.goto('https://google.com');
-    const searchBox = page.locator("//div[@class='test']");
-    searchBox.click();
-    await page.locator('.g').then(results => console.log(`Found ${results.length} results`));
+
     // Converted Code End
 });
